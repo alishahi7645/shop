7f6b0c2905ef5f2fecc217d34d1cbd2dbca01f53
alishahi <shahy8227@gmail.com>
2024-09-19 01:42:47 +0330

    end navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,9 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">
-        Navbar
-      </a>
+    <nav className="navbar navbar-expand-lg navbar-light navbar-style p-3">
       <button
         className="navbar-toggler"
         type="button"
@@ -18,64 +16,50 @@ function Navbar() {
         <span className="navbar-toggler-icon"></span>
       </button>
 
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <div
+        className="collapse navbar-collapse d-flex align-center justify-content-between"
+        id="navbarSupportedContent"
+      >
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
-            <a className="nav-link" href="#">
-              Home <span className="sr-only">(current)</span>
-            </a>
+            <NavLink className="nav-link text-white" activClassName='active' to="/dashboard">
+              <i className="fa fa-dashboard"></i> Dashboard
+            </NavLink>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">
-              Link
-            </a>
-          </li>
-          <li className="nav-item dropdown">
-            <a
-              className="nav-link dropdown-toggle"
-              href="#"
-              id="navbarDropdown"
-              role="button"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              Dropdown
-            </a>
-            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a className="dropdown-item" href="#">
-                Action
-              </a>
-              <a className="dropdown-item" href="#">
-                Another action
-              </a>
-              <div className="dropdown-divider"></div>
-              <a className="dropdown-item" href="#">
-                Something else here
-              </a>
-            </div>
+            <NavLink className="nav-link text-white" activClassName='active' to="/login">
+              Login
+            </NavLink>
           </li>
           <li className="nav-item">
-            <a className="nav-link disabled" href="#">
-              Disabled
-            </a>
+            <NavLink className="nav-link text-white" activClassName='active' to="/register">
+              Register
+            </NavLink>
           </li>
         </ul>
-        <form classNameName="form-inline my-2 my-lg-0">
-          <input
-            className="form-control mr-sm-2"
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
-          />
-          <button
-            className="btn btn-outline-success my-2 my-sm-0"
-            type="submit"
-          >
-            Search
-          </button>
-        </form>
+        <div style={{ marginRight: 60 }}>
+          <ul className="navbar-nav">
+            <li className="nav-item dropdown">
+              <NavLink
+                className="nav-link dropdown-toggle text-white"
+                to="/"
+                id="navbarDropdown"
+                role="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                <i className="fa fa-user circle"/>user
+              </NavLink>
+              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                fd
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
+
+      <h2 className="text-white">ShopAli</h2>
     </nav>
   );
 }
